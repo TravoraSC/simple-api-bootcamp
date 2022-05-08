e7fed1a5ebca292f3ad1a1a3cec83d3696af7669
Travora Senecharles <travora9@gmail.com>
2022-05-08 19:36:04 -0400

    I fixed up my CSS but I'm no done yet.

---
+++ main.js
@@ -0,0 +1,22 @@
+  let url = 'https://zoo-animal-api.herokuapp.com/animals/rand'
+
+  fetch (url)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    document.querySelector('.nameAndLatinName').innerText = data.name + ' - ' + data.latin_name  
+    document.querySelector('img').src = data.image_link
+    document.querySelector('.animalType').innerText = 'Animal Type: ' + data.animal_type
+    document.querySelector('.lifespan').innerText = 'Lifespan: ' + data.lifespan
+    document.querySelector('.geoRange').innerText = 'Georange: ' + data.geo_range
+    document.querySelector('.habitat').innerText = 'Habitat: ' + data.habitat
+    document.querySelector('.lengthMin').innerText = 'Length: ' + data.length_min
+    document.querySelector('.lengthMax').innerText = data.length_max
+    document.querySelector('.activeTime').innerText = 'Active Time: ' + data.active_time
+    document.querySelector('.diet').innerText = 'Diet: ' + data.diet
+    document.querySelector('.weightMin').innerText = 'Weight: ' + data.weight_min
+    document.querySelector('.weightMax').innerText = data.weight_max
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
